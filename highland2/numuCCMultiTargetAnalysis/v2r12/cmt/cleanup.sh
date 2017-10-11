c5acac3e73af031feb38720d5d95ee7b354d86b5
Matt Hogan <hoganman@rams.colostate.edu>
2017-10-11 00:55:43 -0600

    remove build specific objects

--- highland2/numuCCMultiTargetAnalysis/v2r12/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/physics/software/CMT/v1r20p20081118; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=numuCCMultiTargetAnalysis -version=v2r12 -path=/physics/home/mhogan/software/t2k-software-clone/highland2 $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
--- nd280Psyche/v3r26/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/physics/software/CMT/v1r20p20081118; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=nd280Psyche -version=v3r26 -path=/physics/home/mhogan/software/t2k-software-clone $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
