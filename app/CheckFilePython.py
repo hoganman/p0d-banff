0cbdce84045b022f2494a7a3dc59e8c78c807db8
Matt Hogan <hoganman@rams.colostate.edu>
2017-11-15 10:24:47 -0700

    reorangiztion

---
+++ app/CheckFilePython.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+""" Check a ROOT file if it has any bad qualities
+    checkfile(infilename) will return an int
+        return 0 (False) if it is bad
+        return 1 (True) if it is good
+    checkfileandexit(fileName) will exit with the same
+        return status as ChechFile(infilename)
+
+"""
+
+from sys import exit
+from FileTools import ROOTFile
+
+
+def checkfile(infilename=''):
+    """Checks if the file is bad (0) or good (1)"""
+    test_file = ROOTFile(infilename)
+    return test_file.status
+
+
+def checkfileandexit(infilename):
+    """Exits with 0 if bad ROOT file or 1 if it is good"""
+    status = checkfile(infilename)
+    exit(status)
--- app/FileTools.py
+++ app/FileTools.py
@@ -1,6 +1,7 @@
 """A general purpose tool to extract text file info"""
 
 from os.path import isfile
+from ROOT import TFile
 
 
 class File(object):
@@ -91,8 +92,42 @@ class TextFile(File):
         return status
 
 
-# class ROOTFile(File):
-#   """Basically to check """
+class ROOTFile(File):
+    """Basically to check if a file exists"""
+
+    def __init__(self, input_file_name=''):
+        super(ROOTFile, self).__init__(input_file_name)
+        self.status = self.checkfile()
+
+    def checkfile(self):
+        """Checks if the file is bad (0) or good (1)"""
+        if not self.exists():
+            return self.IS_BAD
+        infile = None
+        status = self.INDETERMINATE
+        try:
+            infile = TFile.Open(self.get_file_name())
+        except IOError:
+            status = self.IS_BAD
+            infile = None
+        if not infile or not infile.IsOpen():
+            status = self.IS_BAD
+        elif infile.IsRaw():
+            status = self.IS_BAD
+        elif not infile.IsBinary():
+            status = self.IS_BAD
+        elif not infile.GetListOfKeys().GetSize():
+            status = self.IS_BAD
+        elif infile.IsZombie():
+            status = self.IS_BAD
+        elif infile.TestBit(TFile.kRecovered):
+            status = self.IS_BAD
+        else:
+            status = self.IS_GOOD
+        if infile:
+            infile.Close()
+        return status
+
 
 class WriteTextFile(TextFile):
     """The object that writes a text file"""
