d1e85b2a60b39654c469ed1592401903520de91a
Matthew Hogan <hoganman@rams.colostate.edu>
2018-04-06 08:15:06 -0600

    compiler determined by root-config

--- SetupBase.sh
+++ SetupBase.sh
@@ -4,23 +4,13 @@
 OLD_PATH=$PATH
 #######################
 
-export ND280_CPP_COMPILER=$(which g++)
-export ND280_CC_COMPILER=$(which gcc)
-export ND280_FORTRAN_COMPILER=$(which gfortran)
-export ND280VERSION=v11r31
 export ROOTVERSION=v5r34p09n04
 alias gmake="make"
-
-# export S50A="S50-A"
-# export ENSHPC="ens-hpc"
-# export ENSSANDBOX="node7"
-# export HEP="hep"
-# export BKUP="bkup"
 
 if [ $HOSTNAME == $S50A ]; then
     export SOFTWARE=/home/mhogan/software
     export ND280=$SOFTWARE/p0d-banff
-    export PYTHONPATH=.:$HOME:$HOME/bin
+    export PYTHONPATH=.:$HOME:$HOME/bin:$PYTHONPATH
     export IRODSROOT=
     export GOPATH=/home/mhogan/go
 fi
@@ -28,7 +18,7 @@ fi
 if [ $HOSTNAME == $ENSHPC ] || [ $HOSTNAME == $ENSSANDBOX ]; then
     export SOFTWARE=/physics/software
     export ND280=$SOFTWARE/t2k/ND280/v11r31
-    export PYTHONPATH=.:/physics/INSTALLATION/bin:/physics/home/mhogan/bin:/physics/home/mhogan:$HOME:$HOME/bin
+    export PYTHONPATH=.:/physics/INSTALLATION/bin:/physics/home/mhogan/bin:/physics/home/mhogan:$HOME:$HOME/bin:$PYTHONPATH:
     export IRODSROOT=/physics/software/irods-legacy/iRODS
     export GOPATH=/home/other/mhogan/go:/physics/home/mhogan/go
 fi
@@ -36,18 +26,26 @@ fi
 if [ $HOSTNAME == $HEP ]; then
     export SOFTWARE=/Raid/software/t2k/nd280
     export ND280=$SOFTWARE
-    export PYTHONPATH=.:$HOME:$HOME/bin
+    export PYTHONPATH=.:$HOME:$HOME/bin:$PYTHONPATH
     export IRODSROOT=/Raid/software/t2k/irods-legacy/iRODS
     export GOPATH=/home/mhogan/go:/Raid/home/mhogan/go
+    export IRODSROOT=/Raid/software/t2k/irods-legacy/iRODS
 fi
 
 if [ $HOSTNAME == $BKUP ]; then
     export SOFTWARE=/Raid/software/t2k
     export ND280=$SOFTWARE/t2k/nd280
-    export PYTHONPATH=.:$HOME:$HOME/bin
+    export PYTHONPATH=.:$HOME:$HOME/bin:$PYTHONPATH
     export IRODSROOT=
     export GOPATH=/home/mhogan/go
 fi
+
+#if [ ! -z ${IRODSROOT+x} ] ; then
+#    CWD=$PWD
+#    cd $IRODSROOT
+#    source add-clients.sh
+#    cd $CWD
+#fi
 
 # CERNLIB and NEUT
 export CERN_LEVEL=2005
@@ -68,8 +66,13 @@ unset CVS_SERVER
 export CMTPATH=$ND280
 export CMTPROJECTPATH=$(dirname ${ND280})
 
-source $ND280/nd280/$ND280VERSION/cmt/setup.sh
+export ND280VERSION=v11r31
 source $ND280/ROOT/$ROOTVERSION/Linux-x86_64/root/bin/thisroot.sh
+export ND280_CPP_COMPILER=$(which $(eval root-config --cxx))
+export ND280_CC_COMPILER=$(which $(eval root-config --cc))
+export ND280_FORTRAN_COMPILER=$(which $(eval root-config --f77))
+source $ND280/nd280/$ND280VERSION/cmt/setup.sh
+
 
 #######################
 PATH=$OLD_PATH:$PATH
