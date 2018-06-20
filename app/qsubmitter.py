459a244fd195e13f0cbc2c1fe550d287f5885a70
Matt Hogan <hoganman@rams.colostate.edu>
2018-06-20 14:17:10 -0600

    Added check for node state in init

--- app/qsubmitter.py
+++ app/qsubmitter.py
@@ -32,8 +32,8 @@ inOptions = {
 #       h_data    The per-job maximum memory limit in bytes.
 #       h_vmem    The same as h_data (if both are set the minimum is used).
 
-SECONDS_BTN_QSUB = 1
-SECONDS_BTN_RUN = 60
+SECONDS_BTN_QSUB = 15
+SECONDS_BTN_RUN = 20
 csuhpc = -1
 queueTag = '$'
 HIGHLANDIOROOT = os.getenv('HIGHLANDIOROOT')
@@ -151,6 +151,8 @@ def InitNodes():
     	queueName = ''
     	if line.find('.q@') == -1:
     		continue
+        if '-NA-' in line:
+            continue
     	index = line.find('@')-1
     	queueName = line[0:line.find('@')]
     	index = line.find('@')
