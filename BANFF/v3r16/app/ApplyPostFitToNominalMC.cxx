b183a60c34971dad67a9eb070bdf929872e84021
Matt Hogan <hoganman@rams.colostate.edu>
2018-07-20 22:15:18 -0600

    Make samples template (2017 BANFF)

--- BANFF/v3r16/app/ApplyPostFitToNominalMC.cxx
+++ BANFF/v3r16/app/ApplyPostFitToNominalMC.cxx
@@ -14,13 +14,19 @@
 #include "XMLTools.hxx"
 #include <sstream>
 
+inline void Declare(const char* str)
+{
+    std::cout << str << std::endl;
+}
+
 void Usage(const char* programName)
 {
     using namespace std;
     cout << programName << " [INPUTS]" << endl;
     cout << endl;
     cout << "INPUTS" << endl;
-    cout << "    -i path/to/Samples.xml: The path to the XML file containing sample information" << endl;
+    cout << "    -i path/to/Samples.xml: The path to the XML file containing";
+    cout << " sample information" << endl;
     cout << "    -h this message" << endl;
     cout << "    -o outputFile.root: The name of the output ROOT file" << endl;
     exit(0);
@@ -32,10 +38,10 @@ int main(int argc, char** argv){
 
     std::string outputFileName = "";
     std::string inputFileName = "";
-    if(argc > 4){
-        std::cout << "Too many command line arguments!" << std::endl;
-	Usage(argv[0]);
-    }
+    //if(argc > 4){
+    //    std::cout << "Too many command line arguments!" << std::endl;
+    //    Usage(argv[0]);
+    //}
 
     for (;;)
     {
@@ -46,35 +52,35 @@ int main(int argc, char** argv){
         {
             case 'i':
             {
-                outputFileName = optarg;
+                inputFileName = optarg;
                 break;
             }
             case 'h':
             {
-		Usage(argv[0]);
+                Usage(argv[0]);
             }
             case 'o':
             {
-                inputFileName = optarg;
+                outputFileName = optarg;
                 break;
             }
             default:
             {
-		Usage(argv[0]);
+               Usage(argv[0]);
             }
         }
     }//end for
 
     if(outputFileName.length() < 1)
-    {
-	Usage(argv[0]);
-    }
+        Usage(argv[0]);
 
     if(inputFileName.length() < 1)
     {
-	    char* buffer[1000];
         inputFileName = Form("%s/inputs/Binning.xml", getenv("BANFFROOT"));
-	    std::cout << "WARNING: Using " << buffer << "as input file since none was provided!" << std::endl;
+        std::cout << "WARNING: Using "
+                  << inputFileName.c_str()
+                  << " as input file since none was provided!"
+                  << std::endl;
     }
 
     //If there is 1 command line argument, it is specifying a parameter
@@ -89,17 +95,37 @@ int main(int argc, char** argv){
     }
 
     ND::params().SetReadParamOverrideFilePointPassed();
-    const Bool_t throwMCStat  = static_cast<Bool_t>(ND::params().GetParameterI("BANFF.RunFit.ThrowMCStat"));
-    const Bool_t throwStat    = static_cast<Bool_t>(ND::params().GetParameterI("BANFF.RunFit.ThrowStat"));
+    Declare("Loading parameters!");
+    bool throwMCStat              = ND::params().GetParameterI("BANFF.RunFit.ThrowMCStat");
+    bool throwStat                = ND::params().GetParameterI("BANFF.RunFit.ThrowStat");
+    bool loadXSecParams           = ND::params().GetParameterI("BANFF.RunFit.LoadXSecParams");
+    bool loadObsNormParams        = ND::params().GetParameterI("BANFF.RunFit.LoadObsNormParams");
+    bool loadFluxParams           = ND::params().GetParameterI("BANFF.ThrowToys.LoadFluxParams");
+    bool throwXSecParams          = ND::params().GetParameterI("BANFF.ThrowToys.ThrowXSecParams");
+    bool loadNuModeFluxParams     = ND::params().GetParameterI("BANFF.ThrowToys.LoadNuModeFluxParams");
+    bool loadAntiNuModeFluxParams = ND::params().GetParameterI("BANFF.ThrowToys.LoadAntiNuModeFluxParams");
+    bool throwFluxParams          = ND::params().GetParameterI("BANFF.ThrowToys.ThrowFluxParams");
+    bool throwObsNormParams       = ND::params().GetParameterI("BANFF.ThrowToys.ThrowObsNormParams");
+    bool loadDetParams            = ND::params().GetParameterI("BANFF.ThrowToys.LoadDetParams");
+    bool throwDetParams           = ND::params().GetParameterI("BANFF.ThrowToys.ThrowDetParams");
+    std::string fluxInputFile     = ND::params().GetParameterS("BANFF.FluxInputFile");
+    std::string xSecInputFile     = ND::params().GetParameterS("BANFF.XSecInputFile");
+    std::string obsNormInputFile  = ND::params().GetParameterS("BANFF.ObsNormInputFile");
+
+    const std::string pBinsEdges = "pBinsEdges";
+    const std::string ctBinsEdges = "ctBinsEdges";
+    const std::string pBinsEdgesN = "pBinsEdgesN";
+    const std::string ctBinsEdgesN = "ctBinsEdgesN";
+    const std::string SampleId = "SampleId";
 
     //Create the observables we will use.
     psycheLeptonCandidateMomentum* pmu  = new psycheLeptonCandidateMomentum();
     psycheLeptonCandidateCosTheta* thmu = new psycheLeptonCandidateCosTheta();
 
+    Int_t nObs = 2;
     BANFFObservableBase** observables = new BANFFObservableBase*[2];
     observables[0] = pmu;
     observables[1] = thmu;
-    Int_t nObs = 2;
 
     //For each observable and sample set up the binning used in the fit.
 
@@ -107,67 +133,77 @@ int main(int argc, char** argv){
     XMLTools xml(inputFileName.c_str());
     std::map<TString, XMLTools::AttributeMap> samples_attributes = xml.GetAllNodes();
     std::vector<BANFFBinnedSample> banffsamplesVect;
-    for(std::map<TString, XMLTools::AttributeMap>::iterator map_iter = samples_attributes.begin();
+    std::map<TString, XMLTools::AttributeMap>::iterator map_iter;
+    for(map_iter  = samples_attributes.begin();
         map_iter != samples_attributes.end();
         ++map_iter
        )
     {
         const char* sample_name = map_iter->first.Data();
+        Declare(Form("Loading sample %s...", sample_name));
         XMLTools::AttributeMap attributes = map_iter->second;
-        SampleId::SampleEnum sampleId = SampleId::ConvertSample(attributes["SampleId"].Data());
+        SampleId::SampleEnum sampleId = SampleId::ConvertSample(attributes[SampleId].Data());
         TAxis* axes[2];
-        std::vector<TString> pBinsEdgesStr = XMLTools::SplitString(attributes["pBinsEdges"]);
-        std::vector<TString> ctBinsEdgesStr = XMLTools::SplitString(attributes["ctBinsEdges"]);
+        std::vector<TString> pBinsEdgesStr = XMLTools::SplitString(attributes[pBinsEdges]);
+        std::vector<TString> ctBinsEdgesStr = XMLTools::SplitString(attributes[ctBinsEdges]);
         std::vector<Double_t> pBinsEdges(pBinsEdgesStr.size());
         std::vector<Double_t> ctBinsEdges(ctBinsEdgesStr.size());
         for(UInt_t index = 0; index < pBinsEdges.size(); ++index)
         {
             pBinsEdges.at(index) = pBinsEdgesStr.at(index).Atof();
         }
-        if(attributes["pBinsEdgesN"].Atoi() != static_cast<Int_t>(pBinsEdges.size()))
+        UInt_t attr_pBinsEdgesN = attributes[pBinsEdgesN.c_str()].Atoi();
+        UInt_t pBinsEdges_size = pBinsEdges.size();
+        if(attr_pBinsEdgesN != pBinsEdges_size)
         {
             std::cout << "ERROR: the number of stated pBins for "
                       << sample_name
-                      << "does not match in the file: "
+                      << " does not match in the file: "
                       << xml.GetXMLFile().Data() << std::endl;
-	    exit(1);
+            exit(1);
         }
         for(UInt_t index = 0; index < ctBinsEdges.size(); ++index)
         {
             ctBinsEdges.at(index) = ctBinsEdgesStr.at(index).Atof();
         }
-        if(attributes["ctBinsEdgesN"].Atoi() != static_cast<Int_t>(ctBinsEdges.size()))
+        UInt_t attr_ctBinsEdgesN = attributes[ctBinsEdgesN.c_str()].Atoi();
+        UInt_t ctBinsEdges_size = ctBinsEdges.size();
+        if(attr_ctBinsEdgesN != ctBinsEdges_size)
         {
             std::cout << "ERROR: the number of stated ctBins for "
                       << sample_name
                       << "does not match in the file: "
                       << xml.GetXMLFile().Data() << std::endl;
-	    exit(1);
+            exit(1);
         }
-        TAxis* pAxis = new TAxis(pBinsEdges.size()-1, &pBinsEdges[0]);
-        TAxis* ctAxis = new TAxis(ctBinsEdges.size()-1, &ctBinsEdges[0]);
+        TAxis* pAxis = new TAxis(pBinsEdges_size-1, &pBinsEdges[0]);
+        TAxis* ctAxis = new TAxis(ctBinsEdges_size-1, &ctBinsEdges[0]);
         axes[0] = pAxis;
         axes[1] = ctAxis;
-	//Create the samples. Supply the:
-    	//name
-    	//sampleID
-    	//Number of observables.
-    	//Array of observables.
-    	//Axes for the histograms.
-    	//Whether to throw MC statistical errors.
-    	//Whether to throw statistical errors
-        BANFFBinnedSample binnedSample(sample_name, sampleId, nObs, observables, axes, throwMCStat, throwStat);
+        //Create the samples. Supply the:
+        //name
+        //sampleID
+        //Number of observables.
+        //Array of observables.
+        //Axes for the histograms.
+        //Whether to throw MC statistical errors.
+        //Whether to throw statistical errors
+        BANFFBinnedSample binnedSample(sample_name, sampleId, nObs,
+                                       observables, axes, throwMCStat,
+                                       throwStat);
         banffsamplesVect.push_back(binnedSample);
-    }
+    } //end loop over sample attribute
 
     Int_t nSamples = banffsamplesVect.size();
+    Declare(Form("There are %d samples loaded!", nSamples));
     std::vector<BANFFSampleBase*> samples(nSamples);
-    for(std::vector<BANFFBinnedSample>::iterator binnedSampl_iter = banffsamplesVect.begin();
+    std::vector<BANFFBinnedSample>::iterator binnedSampl_iter;
+    for(binnedSampl_iter  = banffsamplesVect.begin();
         binnedSampl_iter != banffsamplesVect.end();
         ++binnedSampl_iter
        )
     {
-        const UInt_t index = banffsamplesVect.end() - binnedSampl_iter;
+        const UInt_t index = binnedSampl_iter - banffsamplesVect.begin();
         samples.at(index) = &(*binnedSampl_iter);
     }
 
@@ -179,22 +215,32 @@ int main(int argc, char** argv){
 
     //Load the Flux Parameters.
     //Specify not to vary them.
-    if(ND::params().GetParameterI("BANFF.ThrowToys.LoadFluxParams")){
-        fitParameters->LoadFluxParametersFromFile(ND::params().GetParameterS("BANFF.FluxInputFile"),
-                ND::params().GetParameterI("BANFF.ThrowToys.LoadNuModeFluxParams"),
-                ND::params().GetParameterI("BANFF.ThrowToys.LoadAntiNuModeFluxParams"),
-                ND::params().GetParameterI("BANFF.ThrowToys.ThrowFluxParams"));
+    if(loadFluxParams)
+    {
+        fitParameters->LoadFluxParametersFromFile(fluxInputFile,
+                                                  loadNuModeFluxParams,
+                                                  loadAntiNuModeFluxParams,
+                                                  throwFluxParams
+                                                 );
     }
 
     //Load the XSec Parameters and specify not to vary them.
-    if(ND::params().GetParameterI("BANFF.ThrowToys.LoadXSecParams")){
-        fitParameters->LoadXSecParametersFromFile(((ND::params().GetParameterS("BANFF.XSecInputFile"))), ND::params().GetParameterI("BANFF.ThrowToys.ThrowXSecParams"));
+    if(loadXSecParams)
+    {
+        fitParameters->LoadXSecParametersFromFile(xSecInputFile,
+                                                  throwXSecParams
+                                                 );
     }
 
     //The ability to load observable normalization parameters if so desired,
     //likely for validation purposes.
-    if(ND::params().GetParameterI("BANFF.ThrowToys.LoadObsNormParams")){
-        fitParameters->LoadObsNormParametersFromFile(((ND::params().GetParameterS("BANFF.ObsNormInputFile"))), nSamples, &samples[0], ND::params().GetParameterI("BANFF.ThrowToys.ThrowObsNormParams"));
+    if(loadObsNormParams)
+    {
+        fitParameters->LoadObsNormParametersFromFile(obsNormInputFile,
+                                                     nSamples,
+                                                     &samples[0],
+                                                     throwObsNormParams
+                                                    );
     }
 
     //With the samples, observables, and fit parameters specified, create the
@@ -205,7 +251,9 @@ int main(int argc, char** argv){
     //FitParameters pointer.
     //Whether to load the detector parameters from psyche.
     //Whether to vary the psyche detector parameters.
-    psycheInterface* interface = new psycheInterface(nSamples, &samples[0], fitParameters, ND::params().GetParameterI("BANFF.ThrowToys.LoadDetParams"), ND::params().GetParameterI("BANFF.ThrowToys.ThrowDetParams"));
+    psycheInterface* interface = 0;
+    interface = new psycheInterface(nSamples, &samples[0], fitParameters,
+                                    loadDetParams, throwDetParams, "diagnostic_ignore_me.root");
 
 
     //Create the output file that the plots will be saved into
@@ -215,9 +263,9 @@ int main(int argc, char** argv){
     //Building and save the MC prediction with everything enabled.
     interface->BuildSaveAndResetMCPrediction("all");
 
-    //Garbage collection
     outFile->Close();
 
+    //Garbage collection
     delete interface;
     delete fitParameters;
     delete observables;
