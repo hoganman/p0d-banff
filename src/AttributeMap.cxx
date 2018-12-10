3d590b52af6b9f9c88eac3b225f06db41cdbd1d1
Matthew Hogan <hoganman@rams.colostate.edu>
2018-12-10 12:22:22 +0900

    separate AttributeMap into new class

---
+++ src/AttributeMap.cxx
@@ -0,0 +1,41 @@
+#define ATTRIBUTEMAP_CXX
+
+#include "AttributeMap.hxx"
+ClassImp(AttributeMap)
+#include "P0DBANFFInterface.hxx"
+
+
+TString AttributeMap::GetAttrib(const TString attribKey) const
+{
+    std::map<TString, TString>::const_iterator it = fMap.find(attribKey);
+    if(it != fMap.end())
+        return it->second;
+    return TString("");
+}
+
+Bool_t AttributeMap::GetAttribBool(const TString attribKey) const
+{
+    return GetAttribInt(attribKey) == 1;
+}
+
+Int_t AttributeMap::GetAttribInt(const TString attribKey) const
+{
+    return GetAttrib(attribKey).Atoi();
+}
+
+Double_t AttributeMap::GetAttribDouble(const TString attribKey) const
+{
+    return GetAttrib(attribKey).Atof();
+}
+
+void AttributeMap::AddAttribute(const TString attribKey, const TString attribValue)
+{
+    std::map<TString, TString>::const_iterator it = fMap.find(attribKey);
+    if(it != fMap.end())
+    {
+        const TString warnMsg = TString::Format("Attribute key %s with value %s is being overritten with %s", it->first.Data(), it->second.Data(), attribValue.Data());
+        P0DBANFFInterface::Warning(this, warnMsg);
+    }
+    //printf("%s : %s\n", attribKey.Data(), attribValue.Data());
+    fMap[attribKey.Data()] = attribValue.Data();
+}
